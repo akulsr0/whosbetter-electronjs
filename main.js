3258e9f251e0d5e010c4edf749657cbb6284b539
akulsrivastava <akulsr0@gmail.com>
2019-09-02 12:56:48 +0530

    pushed

---
+++ main.js
@@ -0,0 +1,42 @@
+const electron = require('electron');
+const url = require('url')
+const path = require('path')
+const {app, BrowserWindow, Menu} = electron;
+
+let mainWindow;
+
+app.on('ready', function(){
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 630,
+    })
+    mainWindow.setTitle("Who's Better")
+    mainWindow.loadURL(url.format({
+        pathname: path.join('https://whosbetter.netlify.com'),
+    }))
+
+    const mainMenu = Menu.buildFromTemplate(menuItems);
+    Menu.setApplicationMenu(mainMenu);
+})
+
+const menuItems = [
+    {
+        label: 'File',
+        submenu: [{
+            label: 'Quit',
+            accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+            click() {
+                app.quit()
+            }
+        }]
+    },
+    {
+        label: 'Results',
+        submenu: [{
+            label: 'Open',
+            click(){
+                mainWindow.loadURL('https://whosbetter.netlify.com/rankings')
+            }
+        }]
+    }
+]
